64eadff4cab21f65eb1808d86e58dea908df051b
Tim Babych <tim.babych@gmail.com>
2009-11-15 21:16:33 +0200

    tweaks

--- src/components/fb2reader.js
+++ src/components/fb2reader.js
@@ -19,11 +19,14 @@
 const Ci = Components.interfaces;
 const Cc = Components.classes;
 
+function dumpln(s){
+  dump(s+"\n");
+}
+
 /* workhorse */
 var FictionBook = {
-
     read: function(a,b){
-        return 'http://ya.ru'
+        return 'http://ya.ru';
     }
 
 }
@@ -36,85 +39,105 @@ const FB_READER_CONTRACT_ID = "@mozilla.org/streamconv;1" + FB_READER_CONVERSION
 const FB_READER_COMPONENT_ID = Components.ID("{99889a2c-d14c-11de-a670-d332511dc9a9}");
 
 function FB_Reader(){
-   this.wrappedJSObject = this;
+    this.wrappedJSObject = this;
 };
 
 FB_Reader.prototype = {
-   classDescription: "FictionBook Reader XPCOM Component",
-   classID: FB_READER_COMPONENT_ID,
-   contractID: FB_READER_CONTRACT_ID,
+    classDescription: "FictionBook Reader XPCOM Component",
+    classID: FB_READER_COMPONENT_ID,
+    contractID: FB_READER_CONTRACT_ID,
 
-   _xpcom_categories: [{
-       category: "@mozilla.org/streamconv;1",
-       entry: FB_READER_CONVERSION,
-       value: "FB2 to HTML stream converter"
-       },
+    _xpcom_categories: [{
+        category: "@mozilla.org/streamconv;1",
+        entry: FB_READER_CONVERSION,
+        value: "FB2 to HTML stream converter"
+        },
     {
-       category: "net-content-sniffers"
+        category: "net-content-sniffers"
     }],
 
-   QueryInterface: XPCOMUtils.generateQI([
-       Ci.nsISupports,
-       Ci.nsIStreamConverter,
-       Ci.nsIStreamListener,
-       Ci.nsIRequestObserver,
-       Ci.nsIContentSniffer
-   ]),
+    QueryInterface: XPCOMUtils.generateQI([
+        Ci.nsISupports,
+        Ci.nsIStreamConverter,
+        Ci.nsIStreamListener,
+        Ci.nsIRequestObserver,
+        Ci.nsIContentSniffer
+    ]),
 
-   data: null,
-   listener: null,
-   channel: null,
+    data: null,
+    listener: null,
+    channel: null,
 
-   /* nsIContentSniffer */
-   getMIMETypeFromContent: function(request, data) {
-      if(request instanceof Ci.nsIHttpChannel) {
-         try {
-            var uri = request.QueryInterface(Ci.nsIChannel).URI.spec;
-            if(uri.match(/.*[^=]\.fb2$/g)) {
-               return "application/fb2";
-            }
-         }
-         catch(e) {
-         }
-      }
-   },
-   /* nsIContentSniffer end */
 
-   /* nsIStreamConverter */
-   convert: function(fromStream, fromType, toType, ctxt) {
+    /*
+    * This component works as such:
+    * 1. asyncConvertData captures the listener
+    * 2. onStartRequest fires, initializes stuff, modifies the listener to match our output type
+    * 3. onDataAvailable transcodes the data into a UTF-8 string
+    * 4. onStopRequest gets the collected data and converts it, spits it to the listener
+    */
+
+
+    // nsIContentSniffer::getMIMETypeFromContent
+    getMIMETypeFromContent: function(request, data) {
+       // sets mime type for .fb2 files  
+       dumpln("getMIMETypeFromContent "+request.name);
+       if(request instanceof Ci.nsIHttpChannel) {
+           try {
+               var uri = request.QueryInterface(Ci.nsIChannel).URI.spec;
+               if(uri.match(/.*[^=]\.fb2$/g)) {
+                   dumpln("MIME set to application/fb2");
+                   return "application/fb2";
+               }
+           }
+           catch(e) {
+           }
+       }
+    },
+
+  
+    // nsIStreamConverter::convert
+    // does nothing, just to comply with interface
+    convert: function(fromStream, fromType, toType, ctxt) {
       return fromStream;
-   },
+    },
 
-   asyncConvertData: function(fromType, toType, listener, ctxt) {
-      this.listener = listener;
-   },
-   /* nsIStreamConverter end */
+    // nsIStreamConverter::asyncConvertData
+    // Store the listener passed to us    
+    asyncConvertData: function(fromType, toType, listener, ctxt) {
+        this.listener = listener;
+    },
 
-   /* nsIStreamListener */
-   onDataAvailable: function(request, context, inputStream, offset, count) {
-        // adds additional data to the this.data
+    // nsIRequestObserver::onStartRequest
+    onStartRequest: function(request, context) {
+        // nullifies this.data
+        this.data = "";
+
+        /* sets charset if it is available. for documents loaded from the filesystem, this is not set. */
+        this.charset = request.QueryInterface(Ci.nsIChannel).contentCharset || 'UTF-8';
+
+        this.channel = request;
+        this.channel.contentType = "text/xml";
+        // All our data will be coerced to UTF-8
+        this.channel.contentCharset = "UTF-8";
+
+        this.listener.onStartRequest(this.channel, context);
+    },
+
+
+
+    // nsIStreamListener::onDataAvailable
+    // adds additional data to the this.data
+    onDataAvailable: function(request, context, inputStream, offset, count) {
+        // From https://developer.mozilla.org/en/Reading_textual_data
+
 
 		var binaryInputStream = Cc["@mozilla.org/binaryinputstream;1"].createInstance(Ci.nsIBinaryInputStream);
 		binaryInputStream.setInputStream(inputStream);
         this.data += binaryInputStream.readBytes(count);
-   },
-   /* nsIStreamListener end */
-
-   /* nsIRequestObserver */
-   onStartRequest: function(request, context) {
-      // nullifies this.data
-      this.data = "";
-
-      /* sets charset if it is available. for documents loaded from the filesystem, this is not set. */
-      this.charset = request.QueryInterface(Ci.nsIChannel).contentCharset || 'UTF-8';
-
-      this.channel = request;
-      this.channel.contentType = "text/xml";
-      this.channel.contentCharset = "UTF-8";
-
-      this.listener.onStartRequest(this.channel, context);
-   },
-
+    },
+ 
+   // nsIRequestObserver::onStopRequest
    onStopRequest: function(request, context, statusCode) {
       var uri = request.QueryInterface(Ci.nsIChannel).URI.spec;
       var output = "";
@@ -136,15 +159,14 @@ FB_Reader.prototype = {
       this.listener.onDataAvailable(this.channel, context, instream, 0, storage.length - trunc);
       this.listener.onStopRequest(request, context, statusCode);
    },
-   /* nsIRequestObserver end */
+
 
 };
 
 /* register component */
 var components = [FB_Reader];
 
-function NSGetModule(compMgr, fileSpec)
-{
+function NSGetModule(compMgr, fileSpec){
    function postRegister() {
       var catMgr = XPCOMUtils.categoryManager;
       catMgr.addCategoryEntry('ext-to-type-mapping', 'fb2', 'application/fb2', true, true);
