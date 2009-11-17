c89bab98548ffcb38728c78195739dc1e5a4d5f9
Tim Babych <tim.babych@gmail.com>
2009-11-17 17:29:12 +0200

    images and notes

--- src/content/overlay.js
+++ src/content/overlay.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function() { fb2Handler.init(); }, false);
 
+const FB2_NS   = 'http://www.gribuser.ru/xml/fictionbook/2.0'
+const XLink_NS = 'http://www.w3.org/1999/xlink';
+const xHTML_NS = 'http://www.w3.org/1999/xhtml';
+const XUL_NS   = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
+
 var fb2Handler = {
   init: function() {
     var appcontent = document.getElementById("appcontent");   // browser
@@ -7,14 +12,21 @@ var fb2Handler = {
       appcontent.addEventListener("DOMContentLoaded", fb2Handler.onPageLoad, true);
   },
 
-  // see https://developer.mozilla.org/en/Xml/id for a reason, why it is needed
-  getBin : function (fbdoc, id) {
-    var bins = fbdoc.getElementsByTagName('binary');
-    for (i in bins){
-        if (bins[i].getAttribute('id') == id) {
-            return bins[i];
-        }
-    }
+  // see https://developer.mozilla.org/en/Xml/id
+  // and http://bit.ly/24gZUo for a reason, why it is needed
+  getElementById : function (doc, id) {
+        return doc.evaluate("//fb2:*[@id='"+id+"']", // namespace-uri()='"+FB2_NS+"' and
+            doc.documentElement,
+            function() {return FB2_NS }, 
+            XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+  },
+
+  // hides XPath query  
+  getElements : function (doc, query) {
+        return doc.evaluate("//fb2:"+query,doc.documentElement,
+                    function(){return FB2_NS},
+                    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
+                    );
   },
 
   onPageLoad: function(event) {
@@ -31,23 +43,27 @@ var fb2Handler = {
         // assign title to the tab
         var lbl = document.getAnonymousElementByAttribute(tab, "class", "tab-text");
         lbl.value = doc.getElementsByTagName("book-title")[0].textContent
-        
-        const XLink_NS = 'http://www.w3.org/1999/xlink';
-        const xHTML_NS = 'http://www.w3.org/1999/xhtml';
-        const XUL_NS   = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
 
-        // for each fb2 image we will create xHTML one
-        images = doc.getElementsByTagName("image");
-        for (i in images){
-            var img = images[i]
+        // for each fb2 image we will create xHTML one        
+        var images = fb2Handler.getElements(doc, "image")
+        for ( var i=0 ; i < images.snapshotLength; i++ ) {
+            var img = images.snapshotItem(i)
             // we get corresponding binary node
-            var bin = fb2Handler.getBin(doc, img.getAttributeNS(XLink_NS, 'href').slice(1));
+            var bin = fb2Handler.getElementById(doc, img.getAttributeNS(XLink_NS, 'href').slice(1));
             // create xhtml image and set src to its base64 data
             var ximg = doc.createElementNS(xHTML_NS, 'img');
             ximg.src='data:'+bin.getAttribute('content-type')+';base64,'+bin.textContent;
             img.parentNode.insertBefore(ximg, img);
-        } 
+        }
         
+        // move all notes inside the links
+        var notelinks = fb2Handler.getElements(doc, "a[@type='note']")
+        for ( var i=0 ; i < notelinks.snapshotLength ; i++ ) {
+            var note = notelinks.snapshotItem(i)
+            var text = fb2Handler.getElementById(doc, note.getAttributeNS(XLink_NS, 'href').slice(1));            
+            note.appendChild(text)
+        }
+
     }
   }
 }
