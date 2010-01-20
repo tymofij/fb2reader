db9c30f2507bf71823ad7dab232fea1c154e2dc3
Tim Babych <tim.babych@gmail.com>
2010-01-21 00:25:07 +0200

    0.8

--- src/content/overlay.js
+++ src/content/overlay.js
@@ -1,76 +1,101 @@
-window.addEventListener("load", function() { fb2Handler.init(); }, false);
+window.addEventListener("load", function() { fb2Handler.init(); }, false)
 
 const FB2_NS   = 'http://www.gribuser.ru/xml/fictionbook/2.0'
-const XLink_NS = 'http://www.w3.org/1999/xlink';
-const xHTML_NS = 'http://www.w3.org/1999/xhtml';
-const XUL_NS   = 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
-
+const XLink_NS = 'http://www.w3.org/1999/xlink'
+const xHTML_NS = 'http://www.w3.org/1999/xhtml'
 
 // utility DOM management functions
 var util = {
+    doc: null, // here we will store the document
+
     // see https://developer.mozilla.org/en/Xml/id
     // and http://bit.ly/24gZUo for a reason why it is needed
-    getElements : function (doc, query, resultType) {
-        if (resultType == null) {
+    getElements : function (query, resultType, prefix) {
+        if (resultType == null)
             resultType = XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE
-        }            
-        return doc.evaluate("//fb2:"+query,doc.documentElement, // could use: namespace-uri()='"+FB2_NS+"' and ..
+        
+        // could use: namespace-uri()='"+FB2_NS+"' and ..
+        return util.doc.evaluate("//fb2:"+query, util.doc.documentElement, 
                     function(){return FB2_NS},
                     resultType, null
                     );
     },
 
-    getSingleElement : function (doc, query) {
-        return util.getElements(doc, query, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue
+    getSingleElement : function (query) {
+        return util.getElements(query, XPathResult.FIRST_ORDERED_NODE_TYPE).singleNodeValue
     },
 
     getHrefVal : function(elem){
-    return elem.getAttributeNS(XLink_NS, 'href').slice(1)  
+        return elem.getAttributeNS(XLink_NS, 'href').slice(1)  
     },
 }
 
 // actual handler
 var fb2Handler = {
     init: function() {
-        var appcontent = document.getElementById("appcontent");   // browser
+        var appcontent = document.getElementById("appcontent") // browser
         if(appcontent)
-            appcontent.addEventListener("DOMContentLoaded", fb2Handler.onPageLoad, true);
+            appcontent.addEventListener("DOMContentLoaded", fb2Handler.onPageLoad, true)
+    },
+
+    tooltip: function(event) {
+        var a = event.target
+        if (a.nodeName=='a'){
+
+            try { // move it here if not yet
+                var note = util.getSingleElement("section[@id='"+util.getHrefVal(a)+"']")                
+                a.appendChild(note)
+            } catch(e) { // just get it
+                var note = a.firstChild
+                while (note.nodeName != 'section')
+                    note = note.nextSibling
+            } 
+
+            // alters the note box's position_h to keep it on screen
+            if ( note.getBoundingClientRect().right > window.innerWidth )
+                note.setAttribute('position_h', 'left')
+            if ( note.getBoundingClientRect().left < 0 )
+                note.setAttribute('position_h', '')
+        }
     },
 
     onPageLoad: function(event) {
         // that is the document that triggered event
-        var doc = event.originalTarget; 
+        var doc = event.originalTarget
+        util.doc = doc
         // execute for FictionBook only
         if(doc.location.href.search(".fb2") > -1) {
-            var browser = gBrowser.getBrowserForDocument(doc);
-            var tabIndex = gBrowser.browsers.indexOf(browser);
-            tab = gBrowser.tabContainer.childNodes[tabIndex]
+            try { // SeaMonkey and Fennec do not have it
+                var browser = gBrowser.getBrowserForDocument(doc)
+                var tabIndex = gBrowser.browsers.indexOf(browser)
+                tab = gBrowser.tabContainer.childNodes[tabIndex]
 
-            // change favicon on the tab
-            var img = document.getAnonymousElementByAttribute(tab, "class", "tab-icon-image");
-            img.src="chrome://fb2reader/skin/icon-16.png";
-            // assign title to the tab
-            var lbl = document.getAnonymousElementByAttribute(tab, "class", "tab-text");
-            lbl.value = doc.getElementsByTagName("book-title")[0].textContent
+                // change favicon on the tab
+                var img = document.getAnonymousElementByAttribute(tab, "class", "tab-icon-image")
+                img.src="chrome://fb2reader/skin/icon-16.png"
+                // assign title to the tab
+                var lbl = document.getAnonymousElementByAttribute(tab, "class", "tab-text")
+                lbl.value = doc.getElementsByTagName("book-title")[0].textContent
+            } catch(e) {}
 
             // for each fb2 image we will create xHTML one        
-            var images = util.getElements(doc, "image")
+            var images = util.getElements("image")
             for ( var i=0 ; i < images.snapshotLength; i++ ) {
-                var img = images.snapshotItem(i)
-                // we get corresponding binary node
-                var bin = util.getSingleElement(doc, "binary[@id='"+util.getHrefVal(img)+"']");
-                // create xhtml image and set src to its base64 data
-                var ximg = doc.createElementNS(xHTML_NS, 'img');
-                ximg.src='data:'+bin.getAttribute('content-type')+';base64,'+bin.textContent;
-                img.parentNode.insertBefore(ximg, img);
+                try { // ignore malformed images
+                    var img = images.snapshotItem(i)
+                    // we get corresponding binary node
+                    var bin = util.getSingleElement("binary[@id='"+util.getHrefVal(img)+"']")
+                    // create xhtml image and set src to its base64 data
+                    var ximg = doc.createElementNS(xHTML_NS, 'img')
+                    ximg.src='data:'+bin.getAttribute('content-type')+';base64,'+bin.textContent
+                    img.parentNode.insertBefore(ximg, img)
+                } catch(e) {}
             }
-            
-            // move all notes inside the links
-            var notelinks = util.getElements(doc, "a[@type='note']")
-            for ( var i=0 ; i < notelinks.snapshotLength ; i++ ) {
+
+            var notelinks = util.getElements("a[@type='note']")
+            for ( var i=0 ; i < notelinks.snapshotLength; i++ ) {
                 var note = notelinks.snapshotItem(i)
-                var text = util.getSingleElement(doc, "section[@id='"+util.getHrefVal(note)+"']");            
-                note.appendChild(text)
+                note.addEventListener("mouseover", fb2Handler.tooltip, true)
             }
         }
     }
