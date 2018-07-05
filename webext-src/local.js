6446e565e8eb7b0642fcab0b83922490c629a94a
Tim Babych <tim.babych@gmail.com>
2018-07-05 00:11:34 -0700

    Google Chrome support

--- webext-src/local.js
+++ webext-src/local.js
@@ -1,29 +1,35 @@
-let txt_html_doc = `
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.gribuser.ru/xml/fictionbook/2.0">
-<head>
-<title>Untitled Fb2</title>
-<link rel="icon" sizes="16x16" href="${browser.extension.getURL('icons/icon-16.png')}" type="image/png" />
-<link rel="icon" sizes="32x32" href="${browser.extension.getURL('icons/icon-16-2x.png')}" type="image/png" />
-<style>
-  @import url(${browser.extension.getURL('css/fb2.css')});
-  @import url(${browser.extension.getURL('css/html.css')});
-  @import url(${browser.extension.getURL('css/print.css')}) print;
-</style>
-</head>
-<body>
-<div id="contents"><div>§</div></div>
-</body>
-</html>`
-
 let parser = new DOMParser();
-let bookTree = document.documentElement
-let bookHTML = parser.parseFromString(txt_html_doc, 'application/xml');
+
+let book_text = document.body.getElementsByTagName('pre')[0].textContent
+let bookTree = parser.parseFromString(book_text, 'application/xml').documentElement;
+document.body.innerHTML = '<div id="contents"><div>§</div></div>'
+let FbInHTML = document.adoptNode(bookTree);
+document.body.appendChild(FbInHTML);
 
 let title_tags = bookTree.getElementsByTagName("book-title")
 if (title_tags.length != 0) {
     let title = title_tags[0].textContent;
-    bookHTML.getElementsByTagName('title')[0].textContent = title;
+    document.title = title;
     console.log("FB title found: " + title);
 }
-bookTree.appendChild(bookHTML.documentElement)
+
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+document.head.appendChild(htmlToElement(`
+    <link rel="icon" sizes="16x16" href="${chrome.runtime.getURL('icons/icon-16.png')}" type="image/png" />
+`))
+document.head.appendChild(htmlToElement(`
+    <link rel="icon" sizes="32x32" href="${chrome.runtime.getURL('icons/icon-16-2x.png')}" type="image/png" />
+`))
+document.head.appendChild(htmlToElement(`
+    <style>@import url(${chrome.runtime.getURL('css/print.css')}) print;</style>
+`))
+
+
 fb2.init(document)
+
